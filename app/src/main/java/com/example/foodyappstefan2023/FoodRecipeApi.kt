ceade57977e25ab5f6276fb1fa4ec88223d79a4d
jameermulani <jameer.mulani@promobitech.com>
2023-08-07 22:25:42 +0530

    Added FoodRecipeApi

---
+++ app/src/main/java/com/example/foodyappstefan2023/FoodRecipeApi.kt
@@ -0,0 +1,16 @@
+package com.example.foodyappstefan2023
+
+import com.example.foodyappstefan2023.data.models.FoodRecipe
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface FoodRecipeApi {
+
+    @GET("/recipes/complexSearch")
+    suspend fun getRecipes(
+        //path queries
+        @QueryMap queries: Map<String, String>
+    ): Response<FoodRecipe>
+
+}
