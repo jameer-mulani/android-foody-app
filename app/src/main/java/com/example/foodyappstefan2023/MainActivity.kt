6c7a18d9e16b3af8b0aff0dd575a85bbb74bdebc
jameermulani <jameer.mulani@promobitech.com>
2023-08-07 19:46:15 +0530

    Added default navigation graph as my_nav.xml. Added 3 destinations fragment recipes, favorites and food joke. Designed MainActivity layout with NavHostFragment and BottomNavigationView. Created a bottomNavMenu to bottomNavigationView. Connect the bottomNavView with NavController inside MainActivity.

--- app/src/main/java/com/example/foodyappstefan2023/MainActivity.kt
+++ app/src/main/java/com/example/foodyappstefan2023/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.foodyappstefan2023
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+        bottomNavigationView = findViewById(R.id.bottomNavView)
+        bottomNavigationView.setupWithNavController(navController)
+
+
+        //to reflect the fragment label into the default actionbar
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.receipesFragment,
+            R.id.favoritesReceipes,
+            R.id.foodJokeFragment
+        ))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
