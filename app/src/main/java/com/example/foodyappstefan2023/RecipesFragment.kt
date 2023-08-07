7518983d68eead83456ab7944d9e13d801738409
jameermulani <jameer.mulani@promobitech.com>
2023-08-07 21:51:18 +0530

    implemented design for recipe list row item

--- app/src/main/java/com/example/foodyappstefan2023/RecipesFragment.kt
+++ app/src/main/java/com/example/foodyappstefan2023/RecipesFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.cooltechworks.views.shimmer.ShimmerRecyclerView
 
 
 class RecipesFragment : Fragment() {
@@ -14,7 +16,10 @@ class RecipesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_receipes, container, false)
+        val view =  inflater.inflate(R.layout.fragment_receipes, container, false)
+
+
+        return view;
     }
 
 }
